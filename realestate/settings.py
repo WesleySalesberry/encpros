883e577f18f81bfc9cf38eadba94aa1808b77c4e
Wes <wesleySalesberry@hotmail.com>
2021-02-09 17:00:36 -0500

    static files again

--- realestate/settings.py
+++ realestate/settings.py
@@ -145,14 +145,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
 
-# STATICFILES_DIRS = (
-#     os.path.join(BASE_DIR, 'static'),
-# )
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 # Media Folder Setting
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
