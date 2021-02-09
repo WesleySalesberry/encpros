6f8fb84edd70bd2284cb393bcd7e8ef00012bae8
Wes <wesleySalesberry@hotmail.com>
2021-02-09 17:50:04 -0500

    issue with template not found error

--- realestate/settings.py
+++ realestate/settings.py
@@ -76,7 +76,7 @@ ROOT_URLCONF = 'realestate.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
