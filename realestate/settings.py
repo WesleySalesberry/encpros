0a3331783e31464777b5d9a8d66bb0ed8f60d6ec
Wes <wesleySalesberry@hotmail.com>
2021-02-09 17:59:35 -0500

    removed extra template

--- realestate/settings.py
+++ realestate/settings.py
@@ -76,8 +76,7 @@ ROOT_URLCONF = 'realestate.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
-        'DIRS': [BASE_DIR + "/templates", ],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
