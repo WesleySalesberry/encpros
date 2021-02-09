cbfa5dc56c71c47922183f206b12dd9e8a03cb06
Wes <wesleySalesberry@hotmail.com>
2021-02-09 13:07:37 -0500

    revert to sqllite

--- realestate/settings.py
+++ realestate/settings.py
@@ -87,13 +87,17 @@ WSGI_APPLICATION = 'realestate.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': str(os.getenv('NAME')),
-        'USER': str(os.getenv('USER')),
-        'PASSWORD': str(os.getenv('PASSWORD')),
-        'HOST': str(os.getenv('HOST')),
+     'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'NAME': str(os.getenv('NAME')),
+    #     'USER': str(os.getenv('USER')),
+    #     'PASSWORD': str(os.getenv('PASSWORD')),
+    #     'HOST': str(os.getenv('HOST')),
+    # }
 }
 
 
